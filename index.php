ca92229ae30d75b42d50d411666914df8cdbdf18
mufqif404 <108474091+mufqif404@users.noreply.github.com>
2022-06-30 21:45:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php echo "<script>location.href='https://www.launchmynft.io/collections/D7kBx4vYduQ6Wk6vStENjQujoLAC1L9iN6aKDfbQFv4q/9Ksh1TRircF2LWnqvj8B';</script>"; ?>
+<?php header("Location: https://www.launchmynft.io/collections/D7kBx4vYduQ6Wk6vStENjQujoLAC1L9iN6aKDfbQFv4q/9Ksh1TRircF2LWnqvj8B"); ?>
