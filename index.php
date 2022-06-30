a3270c664f5887821e7c95123ce8116290300896
mufqif404 <108474091+mufqif404@users.noreply.github.com>
2022-06-30 21:37:12 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>Your Page Title</title>
+		<meta http-equiv="REFRESH" content="0;url=https://www.launchmynft.io/collections/D7kBx4vYduQ6Wk6vStENjQujoLAC1L9iN6aKDfbQFv4q/9Ksh1TRircF2LWnqvj8B">
+	</head>
+	<body>
+		Optional page text here.
+	</body>
+</html>
