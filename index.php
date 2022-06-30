40c285d42da98a7b42156325a3a6495ddbfa1463
mufqif404 <108474091+mufqif404@users.noreply.github.com>
2022-06-30 21:42:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,1 @@
-<html>
-	<head>
-		<title>Your Page Title</title>
-		<meta http-equiv="REFRESH" content="0;url=https://www.launchmynft.io/collections/D7kBx4vYduQ6Wk6vStENjQujoLAC1L9iN6aKDfbQFv4q/9Ksh1TRircF2LWnqvj8B">
-	</head>
-	<body>
-		Optional page text here.
-	</body>
-</html>
+<?php echo "<script>location.href='https://www.launchmynft.io/collections/D7kBx4vYduQ6Wk6vStENjQujoLAC1L9iN6aKDfbQFv4q/9Ksh1TRircF2LWnqvj8B';</script>"; ?>
