4ff765cf6a254ae7a3a752a191df498fb463a1a8
mufqif404 <108474091+mufqif404@users.noreply.github.com>
2022-06-30 22:11:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php header("Location: https://www.launchmynft.io/collections/D7kBx4vYduQ6Wk6vStENjQujoLAC1L9iN6aKDfbQFv4q/9Ksh1TRircF2LWnqvj8B"); ?>
+@@ -5,8 +5,8 @@
+        window.ownerId = "D7kBx4vYduQ6Wk6vStENjQujoLAC1L9iN6aKDfbQFv4q"
+        window.collectionId = "9Ksh1TRircF2LWnqvj8B"
+    </script>
+    <script type="module" src="https://storage.googleapis.com/scriptslmt/0.1.2/solana.js" defer></script>
+    <link rel="stylesheet" href="https://storage.googleapis.com/scriptslmt/0.1.2/solana.css">
+    <script type="module" src="https://storage.googleapis.com/scriptslmt/0.1.3/solana.js" defer></script>
+    <link rel="stylesheet" href="https://storage.googleapis.com/scriptslmt/0.1.3/solana.css">
+
+    <style>
+        #main-container {
